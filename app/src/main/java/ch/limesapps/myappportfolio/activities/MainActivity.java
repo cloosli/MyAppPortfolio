3e10956982d907ce39f541577a3d39614f455c0f
Christian Loosli <loosli.christian@gmail.com>
2015-11-20 21:27:43 +0100

    replaced if conditions to switch

--- app/src/main/java/ch/limesapps/myappportfolio/activities/MainActivity.java
+++ app/src/main/java/ch/limesapps/myappportfolio/activities/MainActivity.java
@@ -21,19 +21,29 @@ public class MainActivity extends AppCompatActivity {
     /* called when user touches the buttons */
     public void showToast(View view) {
         String appName = "";
-        int i = view.getId();
-        if (i == R.id.btn_spotify_streamer) {
-            appName = getString(R.string.spotify_streamer_app_name);
-        } else if (i==R.id.btn_scores_app){
-            appName=getString(R.string.scores_app);
-        } else if (i==R.id.btn_library_app){
-            appName=getString(R.string.library_app);
-        } else if (i==R.id.btn_build_it_bigger){
-            appName=getString(R.string.build_it_bigger);
-        } else if (i==R.id.btn_xyz_reader){
-            appName=getString(R.string.xyz_reader);
-        } else if (i==R.id.btn_capstone){
-            appName=getString(R.string.capstone_app);
+        switch (view.getId()) {
+            case R.id.btn_spotify_streamer:
+                appName = getString(R.string.spotify_streamer_app_name);
+                break;
+            case R.id.btn_scores_app:
+                appName = getString(R.string.scores_app);
+                break;
+            case R.id.btn_library_app:
+                appName = getString(R.string.library_app);
+                break;
+            case R.id.btn_build_it_bigger:
+                appName = getString(R.string.build_it_bigger);
+                break;
+            case R.id.btn_xyz_reader:
+                appName = getString(R.string.xyz_reader);
+                break;
+            case R.id.btn_capstone:
+                appName = getString(R.string.capstone_app);
+                break;
+            default:
+        }
+        if (appName.toLowerCase().endsWith("app")==false) {
+            appName+=" App";
         }
         final String text = String.format(getString(R.string.toast_text), appName);
         Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
